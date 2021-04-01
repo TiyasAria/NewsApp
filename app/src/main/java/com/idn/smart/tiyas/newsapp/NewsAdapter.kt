78c1147eb0ab576522f7f1f89e84c5ef92f0146b
Tiyas Aria Pratiwi <71452274+TiyasAria@users.noreply.github.com>
2021-04-01 20:39:42 +0700

    New commit

--- app/src/main/java/com/idn/smart/tiyas/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/idn/smart/tiyas/newsapp/NewsAdapter.kt
@@ -7,7 +7,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.idn.smart.tiyas.newsapp.model.Article
 
 /**
-nah disini kita akan membuat class adapter karena dia menggunakan recycler maka 
+nah disini kita akan membuat class adapter karena dia menggunakan recycler maka kita extend (:) ke recycler, dan karena dia
+ merupakan adpter maka .adapter, karena adapter ini merupakan jembatan dari tampilan xml ke logic maka ,akan ada data yang
+ dimasukkan maka kita buat colection <>, dan isinya nama adapter kita , dan . nama class holder kita, karena recycler ini megang
+ view kita , maka harus terdefinisikam dg membuat class holder yang namanya kita kasih .NewsViewHolder, habis tu implement meber
+ dan muncul fun ovveride 3 itu, habis tu class holder nya msih merah karena kita blm buat class nya jadi sekarang kita buat class
+ viewholder nya , inner class NewsViewHolder yang parameternya isinya view , dan extend ke recyclerview.viewholder kita panggil
+ parameter viewnya, lalu jangn lupa untuk yg class nnewsadapter kita kasih param context (isinya), dan datanya yang berupa listNews
+ lalu kita panggil List<class modelnya> agr mencegah nullable maka tambahkan (?)
  */
 
 class NewsAdapter(var context: Context, var listNews : List<Article?>?): RecyclerView.Adapter<NewsAdapter.NewsViewHolder>(){
